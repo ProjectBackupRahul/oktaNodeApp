441e541151353109333ca1a158bcc59cc74276da
projectBackupRahul <projectbackuprahul@gmail.com>
2020-09-23 16:43:38 +0530

    added functionality html to PDF

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const puppeteer = require('puppeteer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,8 +13,20 @@ router.get('/', function(req, res, next) {
 
    router.post('/url', function(req, res, next){
      const url = req.body.url;
-     console.log('Hi there !!')
-     console.log(url);
+
+     async function printPDF() {
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.goto(`${url}`, {waitUntil: 'networkidle0'});
+      const pdf = await page.pdf({ format: 'A4' });
+     
+      await browser.close();
+      printPDF.then(pdf => {
+        res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+        res.send(pdf)
+      });
+    }
+
    });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log("Hi there!!")
+  
   res.send('respond with a resource');
   
 });
